3169e17d66207a178205b6e0c2ec349f5140d80e
lcrnog97 <barasmaler@gmail.com>
2019-11-11 18:38:24 +0100

    Some packages added, Gatsby-config modifed cause of new packages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,26 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Web-shop',
+    items: []
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-file-system`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`
+      },
+    },
+  ]
 }
