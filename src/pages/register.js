3be11abf7221eaf9b1be8768622391933f265038
lcrnog97 <barasmaler@gmail.com>
2020-01-09 14:11:10 +0100

    Add content into register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,1 +1,8 @@
-// REGISTER
+import React from "react"
+import Layout from "../components/layout"
+
+export const RegisterPage = () => {
+  return <Layout></Layout>
+}
+
+export default RegisterPage
