688c1893fa1a47608f56006a0e87c8a5f566b50f
Zvonimir <zvone.baric@gmail.com>
2020-01-10 18:30:57 +0100

    Fix footer sticking out

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,10 @@ export const Footer = ({ children }) => (
       bottom: 0,
       width: "100%",
       height: "60px",
+      padding: "0 10px"
+    }}
+  >
+    <div className = "footerContent" style={{
       borderTop: "1px solid rgba(0, 0, 0, 0.125)",
       borderLeft: "1px solid rgba(0, 0, 0, 0.125)",
       borderRight: "1px solid rgba(0, 0, 0, 0.125)",
@@ -16,19 +20,19 @@ export const Footer = ({ children }) => (
       borderRadius: "5px 5px 0px 0px",
       display: "flex",
       justifyContent: "space-between",
-    }}
-  >
-    <img
-      src={ourLogo}
-      width="60px"
-      height="60px"
-      align="center"
-      alt="logo"
-      style={{ margin: "0px 5px", padding: "5px", borderRadius: "50%" }}
-    />
-    <span style={{ alignSelf: "center", margin: "0px 5px" }}>
-      &copy;by maleri$h and Barke
-    </span>
+    }}>
+      <img
+        src={ourLogo}
+        width="60px"
+        height="60px"
+        align="center"
+        alt="logo"
+        style={{ margin: "0px 5px", padding: "5px", borderRadius: "50%" }}
+      />
+      <span style={{ alignSelf: "center", margin: "0px 5px" }}>
+        &copy;by maleri$h and Barke
+      </span>
+    </div>
   </footer>
 )
 
