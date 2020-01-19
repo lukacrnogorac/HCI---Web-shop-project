14c7ea982c31c0aa2cd50171019f5f89c3e51eec
lcrnog97 <barasmaler@gmail.com>
2020-01-20 00:32:10 +0100

    Add search filter implementation

--- src/components/header.js
+++ src/components/header.js
@@ -10,127 +10,129 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 import ourLogo from "../../content/logo.jpg"
 
-export const Header = ({ children }) => (
-  <div
-    className="navigation"
-    style={{
-      backgroundColor: "#28395c",
-      height: "50px",
-      marginBottom: "10px",
-      display: "flex",
-      justifyContent: "space-between",
-    }}
-  >
-    <div className="header-left" style={{ display: "flex" }}>
-      <Nav.Link href="/" style={{ padding: "0px" }}>
-        <img
-          src={ourLogo}
-          width="50px"
-          height="50px"
-          alt="logo"
-          style={{ padding: "5px", borderRadius: "50%" }}
-        />
-      </Nav.Link>
-      <Nav variant="pills" defaultActiveKey="/">
-        <Nav.Item>
-          <Nav.Link
-            href="/"
-            eventKey="Home"
-            style={{
-              color: "white",
-              height: "50px",
-              padding: "0px 20px",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <div id="homeText">Home</div>
-            <FontAwesomeIcon id="homeIcon" icon={faHome} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="About"
-            href="/about"
-            style={{
-              color: "white",
-              height: "50px",
-              padding: "0px 20px",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <div id="aboutText">About</div>
-            <FontAwesomeIcon id="aboutIcon" icon={faAddressCard} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="News"
-            href="/news"
-            style={{
-              color: "white",
-              height: "50px",
-              padding: "0px 20px",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <div id="newsText">News</div>
-            <FontAwesomeIcon id="newsIcon" icon={faNewspaper} />
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </div>
+export const Header = ({ children }) => {
+  return (
     <div
-      className="header-middle"
-      style={{ display: "flex", alignItems: "center" }}
+      className="navigation"
+      style={{
+        backgroundColor: "#28395c",
+        height: "50px",
+        marginBottom: "10px",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
     >
-      <div className="searchbar" style={{ height: "38px" }}>
-        <InputGroup className="mb-3">
-          <FormControl placeholder="Search for product" />
-        </InputGroup>
+      <div className="header-left" style={{ display: "flex" }}>
+        <Nav.Link href="/" style={{ padding: "0px" }}>
+          <img
+            src={ourLogo}
+            width="50px"
+            height="50px"
+            alt="logo"
+            style={{ padding: "5px", borderRadius: "50%" }}
+          />
+        </Nav.Link>
+        <Nav variant="pills" defaultActiveKey="/">
+          <Nav.Item>
+            <Nav.Link
+              href="/"
+              eventKey="Home"
+              style={{
+                color: "white",
+                height: "50px",
+                padding: "0px 20px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <div id="homeText">Home</div>
+              <FontAwesomeIcon id="homeIcon" icon={faHome} />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="About"
+              href="/about"
+              style={{
+                color: "white",
+                height: "50px",
+                padding: "0px 20px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <div id="aboutText">About</div>
+              <FontAwesomeIcon id="aboutIcon" icon={faAddressCard} />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="News"
+              href="/news"
+              style={{
+                color: "white",
+                height: "50px",
+                padding: "0px 20px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <div id="newsText">News</div>
+              <FontAwesomeIcon id="newsIcon" icon={faNewspaper} />
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+      <div
+        className="header-middle"
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <div className="searchbar" style={{ height: "38px" }}>
+          <InputGroup className="mb-3">
+            <FormControl placeholder="Search for product" />
+          </InputGroup>
+        </div>
+      </div>
+      <div
+        className="header-right"
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <Nav style={{ margin: "0px 10px" }}>
+          <Nav.Item>
+            <Button
+              variant="light"
+              onClick={() => {
+                navigate("/login")
+              }}
+              style={{
+                width: "110px",
+                // backgroundColor: "#203765",
+                //  borderColor: "#152442",
+              }}
+            >
+              Log in
+            </Button>
+          </Nav.Item>
+          <Nav.Item>
+            <Button
+              variant="primary"
+              onClick={() => {
+                navigate("/register")
+              }}
+              style={{
+                marginLeft: "10px",
+                width: "110px",
+                //  backgroundColor: "#203765",
+                // borderColor: "#152442",
+              }}
+            >
+              Register
+            </Button>
+          </Nav.Item>
+        </Nav>
       </div>
     </div>
-    <div
-      className="header-right"
-      style={{ display: "flex", alignItems: "center" }}
-    >
-      <Nav style={{ margin: "0px 10px" }}>
-        <Nav.Item>
-          <Button
-            variant="light"
-            onClick={() => {
-              navigate("/login")
-            }}
-            style={{
-              width: "110px",
-              // backgroundColor: "#203765",
-              //  borderColor: "#152442",
-            }}
-          >
-            Log in
-          </Button>
-        </Nav.Item>
-        <Nav.Item>
-          <Button
-            variant="primary"
-            onClick={() => {
-              navigate("/register")
-            }}
-            style={{
-              marginLeft: "10px",
-              width: "110px",
-              //  backgroundColor: "#203765",
-              // borderColor: "#152442",
-            }}
-          >
-            Register
-          </Button>
-        </Nav.Item>
-      </Nav>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Header
--- src/pages/category.js
+++ src/pages/category.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout_with_sidebar.js"
 import { graphql, navigate } from "gatsby"
 import Img from "gatsby-image"
 import Sidebar from "../components/sidebar"
-import { Card, Button } from "react-bootstrap"
+import { Card, Button, InputGroup, FormControl } from "react-bootstrap"
 
 export const Category = ({ data, location }) => {
+  const [search, setSearch] = useState("")
+  const [categoryData, setCategoryData] = useState([])
   if (!location.state) {
     return <Layout></Layout>
   } else {
@@ -41,63 +43,106 @@ export const Category = ({ data, location }) => {
               alignItems: "center",
             }}
           >
-            <Card.Title>Search</Card.Title>
+            <div className="searchbar" style={{ height: "38px" }}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="Search for product"
+                  onKeyUp={e => {
+                    setSearch(e.target.value)
+                    const imageSharp = mdx[0].image.childImageSharp.fluid
+                    mdx = mdx.filter(element => {
+                      const title = element.title.toLowerCase()
+                      if (title.includes(e.target.value.toLowerCase())) {
+                        return element
+                      }
+                    })
+                    if (!mdx.length) {
+                      mdx.push({
+                        id: 1500,
+                        hidden: "hidden",
+                        category: categoryModel.folderName,
+                        description: "Null",
+                        title: "No Title",
+                        image: {
+                          childImageSharp: {
+                            fluid: imageSharp,
+                          },
+                        },
+                      })
+                    }
+                    setCategoryData(mdx)
+                  }}
+                />
+              </InputGroup>
+            </div>
           </Card>
-          <div
-            className="cardGrid"
-            style={{
-              margin: "10px",
-              display: "grid",
-              gridGap: "10px",
-              gridTemplateColumns:
-                "repeat(auto-fit, minmax(274px, max-content))",
-              justifyContent: "center",
-            }}
-          >
-            {mdx.map(element => {
-              return (
-                <Card
-                  style={{ overflow: "hidden" }}
-                  key={element.id}
-                  data-id={element.id}
-                >
-                  <Img
-                    fluid={element.image.childImageSharp.fluid}
-                    className="customImage"
-                    style={{
-                      height: "220px",
-                    }}
-                  />
-                  <Card.Body
-                    style={{
-                      padding: "10px",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Card.Title>{element.title}</Card.Title>
-                    <Card.Text>{element.price}€</Card.Text>
-                    <Button
-                      variant="secondary"
-                      style={{ width: "100%" }}
-                      onClick={() => {
-                        navigate("/article", {
-                          state: { element },
-                        })
-                      }}
-                    >
-                      Purchase
-                    </Button>
-                  </Card.Body>
-                </Card>
-              )
-            })}
-          </div>
+          {pageContent(
+            categoryData.length && categoryData[0].category === mdx[0].category
+              ? categoryData
+              : mdx
+          )}
         </div>
       </Layout>
     )
   }
+}
+
+function pageContent(mdx) {
+  return (
+    <div
+      className="cardGrid"
+      style={{
+        margin: "10px",
+        display: "grid",
+        gridGap: "10px",
+        gridTemplateColumns: "repeat(auto-fit, minmax(274px, max-content))",
+        justifyContent: "center",
+      }}
+    >
+      {mdx.map(element => {
+        return (
+          <Card
+            style={{
+              overflow: "hidden",
+              visibility: element.hidden ? "hidden" : "visible",
+            }}
+            key={element.id}
+            data-id={element.id}
+          >
+            <Img
+              fluid={element.image.childImageSharp.fluid}
+              className="customImage"
+              style={{
+                height: "220px",
+              }}
+            />
+            <Card.Body
+              style={{
+                padding: "10px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <Card.Title>{element.title}</Card.Title>
+              <Card.Text>{element.price}€</Card.Text>
+              <Button
+                variant="secondary"
+                style={{ width: "100%" }}
+                onClick={() => {
+                  navigate("/article", {
+                    state: { element },
+                  })
+                }}
+              >
+                Purchase
+              </Button>
+            </Card.Body>
+          </Card>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Category
