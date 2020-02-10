c53423342496ac141d8e0d5af39c26c2e21a1304
Zvonimir <zvone.baric@gmail.com>
2020-02-10 13:13:38 +0100

    Change news article page

--- src/pages/article-details.js
+++ src/pages/article-details.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
-import { Card, Form } from "react-bootstrap"
+import { Card, Form, Nav } from "react-bootstrap"
+import { faTimes } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import defaultProfile from "../../content/default-profile.png"
+import authorImage from "../../content/author-image.jpg"
 
 const ArticleDetails = ({ location }) => {
   if (!location.state) {
@@ -13,44 +17,92 @@ const ArticleDetails = ({ location }) => {
         <div key={item.id}>
           <Card
             id="articleDetail"
-            style={{ margin: "0 10px 10px", padding: "10px" }}
+            style={{ margin: "0 10px 10px", padding: "0 10px 10px" }}
           >
-            <h1 style={{ marginBottom: "10px" }}>{item.title}</h1>
-            <div className="image-container">
-              <Img
-                fluid={item.image.childImageSharp.fluid}
-                className="customImage"
-                style={{
-                  maxHeight: "480px",
-                  maxWidth: "480px",
-                  borderRadius: "5px",
-                }}
-              />
-            </div>
-            <div className="authorInfo" style={{ marginTop: "10px" }}>
-              <Card.Title>Author Authorized</Card.Title>
-              <Card.Title>Created at: 12.01.2020</Card.Title>
-              <p>
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-                {item.description} This is Description. You can add what ever
-                you want. Just try it!
-              </p>
+            <Nav variant="pills">
+              <Nav.Item>
+                <Nav.Link
+                  href="/news"
+                  style={{ position: "absolute", right: "0%", top:"0%", padding: "0px 5px" }}
+                >
+                  <FontAwesomeIcon
+                    id="cancelIcon"
+                    icon={faTimes}
+                    style={{ color: "#212529" }}
+                  />
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <h1 style={{ marginBottom: "10px"}}>{item.title}</h1>
+            <div id="article-content">
+              <div className="articleBody" style={{ marginTop: "10px"}}>
+                <div id="authorInfo" style={{display: "flex", marginBottom: "10px"}}>
+                <img 
+                  src={authorImage}
+                  width="50px"
+                  height="50px"
+                  style={{padding: "0px", marginRight: "5px", borderRadius: "50%"}}
+                  />
+                  <div>
+                    <Card.Title style={{marginBottom: "2px"}}>lerk97</Card.Title>
+                    <Card.Title>12.01.2020.</Card.Title>
+                  </div>
+                </div>
+                <p>
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                </p>
+                <p>
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it! 
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                  {item.description} This is Description. You can add what ever
+                  you want. Just try it!
+                </p>
+              </div>
+              <div className="image-container">
+                <Img
+                  fluid={item.image.childImageSharp.fluid}
+                  className="customImage"
+                  style={{
+                    maxHeight: "480px",
+                    maxWidth: "480px",
+                    borderRadius: "5px",
+                  }}
+                />
+              </div>
             </div>
           </Card>
-          <Form id="commentArea" style={{ margin: "0px 10px 0px" }}>
-            <Form.Control as="textarea" placeholder="Comment..." rows="3" />
-          </Form>
+          <Card id="commentBox" style={{margin: "0 10px 10px", padding: "0 10px 10px"}}>
+            <h1>Comments</h1>
+            <div id="userComment" style={{display: "flex"}}>
+              <img 
+              src={defaultProfile}
+              width="50px"
+              height="50px"
+              style={{padding: "5px", borderRadius: "50%"}}
+              />
+              <Form id="commentArea" style={{width: "100%" }}>
+                <Form.Control as="textarea" placeholder="Comment..." rows="3" />
+              </Form>
+            </div>
+            <i style={{display: "flex", justifyContent: "center", padding: "20px 0", color: "grey"}}>There doesn't seem to be anything here. Be the first to comment.</i>
+          </Card>
         </div>
       </Layout>
     )
