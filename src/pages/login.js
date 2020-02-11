bc611a60261893acf9ad9392c2cc0ec303cdf71a
lcrnog97 <barasmaler@gmail.com>
2020-02-11 13:25:33 +0100

    Add check for email and password length

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { navigate } from "gatsby"
 import Header from "../components/header.js"
 import Footer from "../components/footer.js"
@@ -8,17 +8,21 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons"
 import { Helmet } from "react-helmet"
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState("")
+  const [passwordLength, setPasswordLength] = useState(0)
   return (
     <div style={{ backgroundColor: "#EAEDED", minHeight: "100vh" }}>
       <Helmet title="Login | Web-shop" />
       <Header />
-      <div style={{
-        position: "relative",
-        minHeight: "100vh",
-        margin: "0 auto",
-        marginTop: "50px",
-        paddingTop: "10px"
-      }}>
+      <div
+        style={{
+          position: "relative",
+          minHeight: "100vh",
+          margin: "0 auto",
+          marginTop: "50px",
+          paddingTop: "10px",
+        }}
+      >
         <Form
           style={{
             margin: "0 auto",
@@ -35,7 +39,11 @@ export const LoginPage = () => {
             <Nav.Item>
               <Nav.Link
                 href="/"
-                style={{ position: "absolute", right: "0%", padding: "0px 5px" }}
+                style={{
+                  position: "absolute",
+                  right: "0%",
+                  padding: "0px 5px",
+                }}
               >
                 <FontAwesomeIcon
                   id="cancelIcon"
@@ -47,20 +55,39 @@ export const LoginPage = () => {
           </Nav>
           <h1 style={{ marginTop: "5px" }}>Log in</h1>
           <Form.Group controlId="formBasicEmail">
-            <Form.Label style={{ marginBottom: "2px" }}>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label style={{ marginBottom: "2px" }}>
+              Email address
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={e => {
+                setEmail(e.target.value)
+              }}
+            />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label style={{ marginBottom: "2px" }}>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={e => {
+                setPasswordLength(e.target.value.length)
+              }}
+            />
           </Form.Group>
           <div>
             <Button
               variant="primary"
               type="submit"
-              onClick={() => {
-                navigate("/")
+              onClick={e => {
+                e.preventDefault()
+                if (passwordLength > 0 && email.length > 0) {
+                  return navigate("/")
+                } else {
+                  return alert("You must enter valid email and password")
+                }
               }}
               style={{ width: "100%" }}
             >
@@ -75,7 +102,10 @@ export const LoginPage = () => {
               }}
             >
               <div>Not a member yet?</div>
-              <Nav.Link href="/register/" style={{ padding: "0px 0px 0px 5px" }}>
+              <Nav.Link
+                href="/register/"
+                style={{ padding: "0px 0px 0px 5px" }}
+              >
                 Register
               </Nav.Link>
             </div>
